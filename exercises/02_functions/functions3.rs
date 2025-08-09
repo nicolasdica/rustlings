a4ea620502213201b8abfe976b33e3adf664d3c5
Nicolas Di Caterina <nicolas.dicaterina8@gmail.com>
2025-08-09 12:33:14 -0300

    Initial commit

---
+++ exercises/02_functions/functions3.rs
@@ -0,0 +1,11 @@
+fn call_me(num: u8) {
+    for i in 0..num {
+        println!("Ring! Call number {}", i + 1);
+    }
+}
+
+fn main() {
+    // TODO: Fix the function call.
+    let num: u8 = 5;
+    call_me(num);
+}
